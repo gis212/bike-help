e1779340d889d1f732360664a3119ab46c15f9f5
gis212 <maxit3569@gmail.com>
2021-10-25 20:52:38 +0300

    improved i18n translate fucntion - updated pug and css - stylus added - WIP

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,44 @@
+let { readFileSync } = require('fs')
+let { load } = require("js-yaml")
+let poststylus = () =>
+  require('poststylus')(['autoprefixer', 'postcss-font-magician', 'cssnano'])
 module.exports = function(grunt) { 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     pug: {
       templates: {
         options: {
+          data: {
+            require,
+            $default_i18n: load(readFileSync('./locales/_default.yaml', 'utf8'))
+          },
           i18n: {
-            locales: 'locales/*.yaml',
+            locales: 'locales/!(_default).yaml',
             namespace: '$i18n',
           },
         },
         files: {
-          'public/index.html': ['index.pug']
+          'public/index.html': ['index.pug'],
         },
+      },
+    },
+    stylus: {
+      compile: {
+        options: { use: [poststylus] },
+        files: { 'public/style.css': 'style.styl' },
       },
     },
     copy: {
       main: {
         files: [
-          ...['css', 'img'].map(dir => ({
-            expand: true, src:[`${dir}/**`], dest: 'public/',
-          })),
-          {expand: true, src:['index.html'], dest: 'public/'},
-        ]
+          { expand: true, src: ['img/**'], dest: 'public/' },
+          { expand: true, src: ['index.html'], dest: 'public/' },
+        ],
       },
     },
   })
   grunt.loadNpmTasks('grunt-pug-i18n')
+  grunt.loadNpmTasks('grunt-contrib-stylus')
   grunt.loadNpmTasks('grunt-contrib-copy')
-  grunt.registerTask('default', ['pug', 'copy'])
+  grunt.registerTask('default', ['pug', 'stylus', 'copy'])
 }
